97c0a9e423772b7176becea572c36f4495b32811
Henrique Lopes <hcmlopes@gmail.com>
2023-10-01 03:52:02 -0400

    test vercel public url

--- src/app/partner/donation/add/page.js
+++ src/app/partner/donation/add/page.js
@@ -32,7 +32,7 @@ export function DonationEventAdd() {
     };
 
     (async function () {
-      const response = await fetch(`/partner/donation/add`, configObj);
+      const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/partner/donation/add`, configObj);
       const donationEvent = await response.json();
       console.log("addPartner Response", donationEvent)
       return donationEvent;
@@ -96,7 +96,7 @@ export function DonationItemAdd() {
   useEffect(() => {
     async function fetchItems() {
       // fetch('/items');
-      const response = await fetch(`/partner/item/list/api`);
+      const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/partner/item/list/api`);
       const fetchedItems = await response.json();
       setItems(fetchedItems);
     }
@@ -132,7 +132,7 @@ export function DonationItemAdd() {
     };
 
     (async function () {
-      const response = await fetch(`/partner/donation/add/api`, configObj);
+      const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/partner/donation/add/api`, configObj);
       const donationItem = await response.json();
       console.log("addDonationItem Response", donationItem)
       return donationItem;
