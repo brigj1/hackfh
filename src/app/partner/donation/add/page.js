5d24d79b43851424928f3be9bf2b91d10eb14c0e
Brigham <brig.johnson@gmail.com>
2023-10-01 01:52:59 -0400

    second bump of donation-add form

--- src/app/partner/donation/add/page.js
+++ src/app/partner/donation/add/page.js
@@ -1,17 +1,13 @@
 'use client'
 
+import { useEffect, useState } from 'react';
+import { NumericFormat } from 'react-number-format';
 import { useForm } from "../../hooks/useForm";
 import { useStyles } from "../../hooks/useStyles";
 import Table from 'react-bootstrap/Table';
 
 export function DonationEventAdd() {
   const { tableShow, tableData } = useStyles();
-
-  function Component() {
-    const [datetime, setDatetime] = useState(() => getInputDatetime()) // getInputDatetime does the formatting of the current date.
-  
-    return <input type='datetime-local' value={datetime} onChange={(e) => setDatetime(e.target.value)}/>
-  }
 
   const initialState = {
     createdAt: '',
@@ -27,7 +23,7 @@ export function DonationEventAdd() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log('formData for addPartner: ', formData);
+    console.log('formData for addDonationEvent: ', formData);
 
     const configObj = {
       method: 'POST',
@@ -36,10 +32,10 @@ export function DonationEventAdd() {
     };
 
     (async function () {
-      const response = await fetch('http://localhost:3000/admin/partner/add/api', configObj);
-      const partner = await response.json();
-      console.log("addPartner Response", partner)
-      return partner;
+      const response = await fetch('http://localhost:3000/partner/donation/add', configObj);
+      const donationEvent = await response.json();
+      console.log("addPartner Response", donationEvent)
+      return donationEvent;
     })();
   }
 
@@ -82,6 +78,135 @@ export function DonationEventAdd() {
                   name="notes"
                   onChange={handleChange}
                   value={formData.notes}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+        <input key="200" type='submit' value='Submit' />
+      </form>
+    </>
+  )
+}
+
+
+export function DonationItemAdd() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function fetchItems() {
+      // fetch('/items');
+      const response = await fetch('http://localhost:3000/partner/item/list/api');
+      const fetchedItems = await response.json();
+      setItems(fetchedItems);
+    }
+
+    fetchItems();
+  }, []);
+
+  const { tableShow, tableData } = useStyles();
+
+  const initialState = {
+    createdAt: new Date(),
+    additionalImages: '',
+    quantity: 1,
+    retailPrice: 0,
+
+    // get id via elsewhere on this form
+    donationEventId: 1,
+
+    itemId: '',
+  };
+
+  const { formData, handleChange, reset, editFormValues } = useForm(initialState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('formData for addDonationItem: ', formData);
+
+    const configObj = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    };
+
+    (async function () {
+      const response = await fetch('http://localhost:3000/partner/donation/add/api', configObj);
+      const donationItem = await response.json();
+      console.log("addDonationItem Response", donationItem)
+      return donationItem;
+    })();
+  }
+
+  return (
+    <>
+      <h1 className="mb-8 text-3xl font-bold">Choose a Donation Date</h1>
+      <form onSubmit={handleSubmit}>
+        <Table responsive striped bordered hover size="sm" style={tableShow}>
+          <thead >
+          </thead>
+          <tbody style={tableData} >
+            <tr>
+              <td>
+                <label htmlFor="theItemId">Item Id</label>
+              </td>
+              <td>
+                <input
+                  key="30"
+                  type="text"
+                  id="theItemId"
+                  name="itemId"
+                  onChange={handleChange}
+                  value={formData.itemId}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="additionalImages">Additional Images</label>
+              </td>
+              <td>
+                <input
+                  key="40"
+                  type="text"
+                  id="additionalImages"
+                  name="additionalImages"
+                  onChange={handleChange}
+                  value={formData.additionalImages}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="donationItemQuantity">quantity</label>
+              </td>
+              <td>
+                <NumericFormat displayType="input"
+                  key="50"
+                  type="text"
+                  id="donationItemQuantity"
+                  name="quantity"
+                  onChange={handleChange}
+                  value={formData.quantity}
+                  decimalScale={0}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="retailPrice">Retail Price</label>
+              </td>
+              <td>
+                &nbsp;$
+                <NumericFormat displayType="input"
+                  key="60"
+                  type="text"
+                  id="retailPrice"
+                  name="retailPrice"
+                  onChange={handleChange}
+                  value={formData.retailPrice}
+                  decimalScale={2}
                 />
               </td>
             </tr>
