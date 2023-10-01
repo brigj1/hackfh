764322d2d53f6b8c48043525f613b71d06d010e5
Henrique Lopes <hcmlopes@gmail.com>
2023-10-01 00:53:05 -0400

    update html property nbames

--- src/app/admin/partner/list/page.js
+++ src/app/admin/partner/list/page.js
@@ -11,28 +11,28 @@ export default async function Page() {
   return (
     <>
       <h1 className="mb-8 text-3xl font-bold">Partner List</h1>
-      <div class="flex flex-col">
-        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-            <div class="overflow-hidden">
-              <table class="min-w-full text-left text-sm font-light">
-                <thead class="border-b font-medium dark:border-neutral-500">
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full text-left text-sm font-light">
+                <thead className="border-b font-medium dark:border-neutral-500">
                   <tr>
-                    <th scope="col" class="px-6 py-4">ID</th>
-                    <th scope="col" class="px-6 py-4">Company Name</th>
-                    <th scope="col" class="px-6 py-4">Contact Name</th>
-                    <th scope="col" class="px-6 py-4">Contact Phone</th>
-                    <th scope="col" class="px-6 py-4">Contact Email</th>
+                    <th scope="col" className="px-6 py-4">ID</th>
+                    <th scope="col" className="px-6 py-4">Company Name</th>
+                    <th scope="col" className="px-6 py-4">Contact Name</th>
+                    <th scope="col" className="px-6 py-4">Contact Phone</th>
+                    <th scope="col" className="px-6 py-4">Contact Email</th>
                   </tr>
                 </thead>
                 <tbody>
                 {partners.map((partner) => (
-                  <tr class="border-b dark:border-neutral-500" key={partner.id}>
-                    <td class="whitespace-nowrap px-6 py-4 font-medium">{partner.id}</td>
-                    <td class="whitespace-nowrap px-6 py-4">{partner.companyName}</td>
-                    <td class="whitespace-nowrap px-6 py-4">{partner.contactName}</td>
-                    <td class="whitespace-nowrap px-6 py-4">{partner.contactPhone}</td>
-                    <td class="whitespace-nowrap px-6 py-4">{partner.contactEmail}</td>
+                  <tr className="border-b dark:border-neutral-500" key={partner.id}>
+                    <td className="whitespace-nowrap px-6 py-4 font-medium">{partner.id}</td>
+                    <td className="whitespace-nowrap px-6 py-4">{partner.companyName}</td>
+                    <td className="whitespace-nowrap px-6 py-4">{partner.contactName}</td>
+                    <td className="whitespace-nowrap px-6 py-4">{partner.contactPhone}</td>
+                    <td className="whitespace-nowrap px-6 py-4">{partner.contactEmail}</td>
                   </tr>
                 ))}
                 </tbody>
